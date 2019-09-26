946cf481f568068f905405f157dbb16b66ed8d17
denyskobzar <denys.kobzar@eonaura.de>
2019-09-27 00:48:26 +0300

    [feature] - extended redis item by additional info;

--- src/core/fsm/states/ContactCollection.state.ts
+++ src/core/fsm/states/ContactCollection.state.ts
@@ -6,6 +6,7 @@ import { Button } from "@core/bots/Bot";
 import { Buttons, Phrases, Translator } from "@core/bots/translator";
 import { Configurator } from "@core/bots/Configurator";
 import { Options } from "../decorators";
+import {Redis} from "@core/Redis";
 
 @Options(StateName.ContactCollection)
 export class ContactCollectionState extends State {
@@ -28,11 +29,11 @@ export class ContactCollectionState extends State {
         let additionalInfo;
         switch (data.command) {
             case Configurator.getButtonValue(Buttons.EMAIL):
-                additionalInfo.contactType = Configurator.getButtonValue(Buttons.EMAIL);
+                additionalInfo = {contactType: Configurator.getButtonValue(Buttons.EMAIL)};
                 await user.updateMood(MoodState.AGREE);
                 break;
             case Configurator.getButtonValue(Buttons.PHONE_NUMBER):
-                additionalInfo.contactType = Configurator.getButtonValue(Buttons.PHONE_NUMBER);
+                additionalInfo = {contactType: Configurator.getButtonValue(Buttons.PHONE_NUMBER)};
                 await user.updateMood(MoodState.AGREE);
                 break;
             default:
@@ -41,7 +42,7 @@ export class ContactCollectionState extends State {
         }
 
         await user.bot.sendMessage(user.botId, `Please type your contact bellow...`);
-        await super.changeState(user, this, additionalInfo);
+        await super.changeState(user, this, Redis.WEEK_TTL, additionalInfo);
     }
 
     protected async reply(user: User, data: IncomingMessage, additional?: any): Promise<void> {
