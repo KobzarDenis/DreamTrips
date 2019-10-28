ceed52a31790e49252aebba409a34dc5e6c00c99
denyskobzar <denys.kobzar@eonaura.de>
2019-10-28 19:08:58 +0200

    [fix] - fixed data persistence;

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ import * as path from "path";
 import {StateHolder} from "@core/fsm";
 import {FileManager} from "@core/fileManager";
 import {Scheduler} from "@core/scheduler";
-import {persistDataFromRedis} from "@core/scheduler/jobs";
 
 sm.install();
 
@@ -33,7 +32,6 @@ sm.install();
         systemBot.init();
         await systemBot.loadAdmins();
         Redis.getInstance(appconfig.redis.url);
-        await persistDataFromRedis();
 
         botRelation[BotName.Facebook] = FacebookBot.getInstance();
         StateHolder.init(botRelation, stateRelation);
